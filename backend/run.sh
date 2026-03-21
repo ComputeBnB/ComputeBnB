f30502ed982ec7df91787351bfa3cc3752b3c8ad
Apps247 <aprameyaithal@gmail.com>
2026-03-21 14:03:25 -0700

    Added setup

--- backend/run.sh
+++ backend/run.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 # Create venv if it doesn't exist
 if [ ! -d ".venv" ]; then
     echo "Creating virtual environment"
-    $REQUIRED_PYTHON -m venv .venv
+    python3 -m venv .venv
 fi
 
 # Activate and install
